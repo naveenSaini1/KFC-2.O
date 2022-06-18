7c4b0e5cc87424a9ccebaeb3474f95d98fc63637
naveenSaini1 <naveensaini94667@gmail.com>
2022-06-18 13:04:51 +0530

    init hompage

--- js/index.js
+++
@@ -1,9 +0,0 @@
-let flag = JSON.parse(localStorage.getItem("flag_value"));
-
-if(flag === true){
-    document.querySelector("#signIn_index").innerText = "";
-    let a = document.createElement("a");
-    a.setAttribute("href","account.html")
-    a.innerText = "Account"
-    document.querySelector("#signIn_index").append(a);
-}
